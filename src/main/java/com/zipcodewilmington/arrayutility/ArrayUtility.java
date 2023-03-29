a93b8d1567b765327c46adfb437315f1e06a2b2f
ule270 <u.le270@gmail.com>
2023-03-28 23:16:58 -0400

    first test attempted.

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,27 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T>{
+    T[] newArr;
+
+    public ArrayUtility(T[] newArr) {
+        this.newArr = newArr;
+    }
+    public Integer countDuplicatesInMerge(T[] inputArray, T valueToEvaluate){
+        int count =  0;
+        for (int i = 0; i < inputArray.length; i++){
+            if (inputArray[i] == valueToEvaluate){
+                count = count +i;
+            }
+        }
+        return count;
+    }
+    public long getNumberOfOccurrences(Integer[] inputArray){
+        return -1;
+    }
+
 }
