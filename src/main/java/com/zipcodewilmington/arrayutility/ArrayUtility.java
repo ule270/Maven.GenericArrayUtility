859f61d8e4119ef13e73e961dea64f4e7a77a60f
ule270 <u.le270@gmail.com>
2023-03-29 21:11:43 -0400

    changes to arrays.

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -6,22 +6,29 @@ import java.util.Arrays;
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T>{
-    T[] newArr;
+    T[] inputArray;
 
-    public ArrayUtility(T[] newArr) {
-        this.newArr = newArr;
+    public ArrayUtility(T[] inputArray) {
+        this.inputArray = inputArray;
     }
-    public Integer countDuplicatesInMerge(T[] inputArray, T valueToEvaluate){
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
+//        merge arrays first then iterate through and count
         int count =  0;
         for (int i = 0; i < inputArray.length; i++){
             if (inputArray[i] == valueToEvaluate){
-                count = count +i;
+                count++;
             }
         }
         return count;
     }
     public long getNumberOfOccurrences(Integer[] inputArray){
         return -1;
+    }
+    public Integer removeValueTest(Integer[] inputArray){
+        return 1;
+    }
+    public Integer getMostCommonFromMergeTest(Integer[] inputArray){
+        return 1;
     }
 
 }
