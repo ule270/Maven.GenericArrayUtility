094dc0d2b72fa640c58d6ceb447f2ed6d9b39604
ule270 <u.le270@gmail.com>
2023-04-01 11:46:08 -0400

    completed. figured out why unable to make empty generic array

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -11,11 +11,20 @@ public class ArrayUtility<T>{
     public ArrayUtility(T[] inputArray) {
         this.inputArray = inputArray;
     }
+
+    public T[] merge(T[] arrayToMerge){
+        T[] mergedArray = Arrays.copyOf(inputArray, inputArray.length + arrayToMerge.length);
+        System.arraycopy(arrayToMerge, 0, mergedArray, inputArray.length, arrayToMerge.length);
+        inputArray = mergedArray;
+        return inputArray;
+    }
+
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
         T[] mergedArray = Arrays.copyOf(inputArray, inputArray.length + arrayToMerge.length);
         System.arraycopy(arrayToMerge, 0, mergedArray, inputArray.length, arrayToMerge.length);
         inputArray = mergedArray;
         return getNumberOfOccurrences(valueToEvaluate);
+//        calling most getNumberOfOccurrences();
 //        int count =  0;
 //        for (int i = 0; i < mergedArray.length; i++){
 //            if (mergedArray[i] == valueToEvaluate){
@@ -33,24 +42,46 @@ public class ArrayUtility<T>{
         }
         return count;
     }
-//    public T removeValueTest(T valueToRemove){
-//        int num = 0;
-//        for (Object element : inputArray) {
-//            if (!element.equals(valueToRemove)) {
-//                num++;
-//            }
-//        }
-//        T [] result = new T [num];
-//        int resultIndex = 0;
-//        for (Object element : inputArray) {
-//            if (!element.equals(valueToRemove)) {
-//                result[resultIndex] = (T) element;
-//                resultIndex++;
-//            }
-//        }
-//        return (T) result;
-//    }
-//    public Integer getMostCommonFromMergeTest(Integer[] inputArray){
-//        return 1;
-//    }
+    public T[] removeValue(T valueToRemove){
+        int num = 0;
+        for (T element : inputArray) {
+            if (!element.equals(valueToRemove)) {
+                num++;
+            }
+        }
+        T [] result = Arrays.copyOf(inputArray, num);
+//        copied array with length of num since;
+//        cannot make empty generic array with length bc no component type;
+//        T[] result = new T [num];
+        int resultIndex = 0;
+        for (T element : inputArray) {
+            if (!element.equals(valueToRemove)) {
+                result[resultIndex] = element;
+                resultIndex++;
+            }
+        }
+        return result;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+//        calling method from above;
+//        T[] mergedArray = Arrays.copyOf(inputArray, inputArray.length + arrayToMerge.length);
+//        System.arraycopy(arrayToMerge, 0, mergedArray, inputArray.length, arrayToMerge.length);
+//        inputArray = mergedArray;
+        T[] mergedArray = merge(arrayToMerge);
+        int maxcount = 0;
+        T maxFreq = null;
+        for (int i = 0; i < mergedArray.length; i++) {
+            int count = 0;
+            for (int j = 0; j < mergedArray.length; j++) {
+                if (mergedArray[i] == mergedArray[j]) {
+                    count++;
+                }
+            }
+            if (count > maxcount) {
+                maxcount = count;
+                maxFreq = mergedArray[i];
+            }
+        } return maxFreq;
+    }
 }
